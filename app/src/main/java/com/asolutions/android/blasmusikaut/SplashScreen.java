94256005402fd083bb0486969315fbc2d8e62b89
asolutions-asturm <asolutions.coding@gmail.com>
2020-08-13 21:45:19 +0200

    startet with splashscreen, documentation folder added

--- app/src/main/java/com/asolutions/android/blasmusikaut/SplashScreen.java
+++ app/src/main/java/com/asolutions/android/blasmusikaut/SplashScreen.java
@@ -1,8 +1,43 @@
 package com.asolutions.android.blasmusikaut;
 
-public class SplashScreen
-{
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
 
-    
+import androidx.appcompat.app.AppCompatActivity;
 
-}
+    public class SplashScreen extends AppCompatActivity
+    {
+        private static int SPLASHSCREEN = 6000;
+
+        Handler handler;
+        Window window;
+        @Override
+        protected void onCreate(Bundle savedInstanceState)
+        {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_splashscreen);
+
+            if(Build.VERSION.SDK_INT >= 21)
+            {
+                window = this.getWindow();
+                window.setStatusBarColor(this.getResources().getColor(R.color.colorBasicWhite));
+            }
+
+            handler = new Handler();
+            handler.postDelayed(new Runnable()
+            {
+                @Override
+                public void run()
+                {
+                    Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                    startActivity(intent);
+                    overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                    finish(); //Closes when 'Back'
+                }
+            }, SPLASHSCREEN);
+        }
+    }
+
